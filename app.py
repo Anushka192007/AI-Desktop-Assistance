d28b04e8daef1c082de17b0546c453c96d8972d7
Anushka192007 <panwaranushka636@gmail.com>
2025-04-13 18:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-gunicorn app:app
+gunicorn app: app
+# app.py
+from flask import Flask
+app = Flask(__name__)  # Make sure this is defined
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+# ... rest of your code
