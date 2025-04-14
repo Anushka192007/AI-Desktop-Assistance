0b5e7c2a2c25ebce8a3adaa8d2d4eb5ae098dc84
Anushka192007 <panwaranushka636@gmail.com>
2025-04-14 18:06:52 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -144,6 +144,8 @@ def retrieve_context_from_ncert(query):
 # Main entry point to run the Flask app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
 
 
